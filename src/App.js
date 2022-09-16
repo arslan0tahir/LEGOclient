c1879740140bac5fd2cc38887bb8474919502cf6
arslan0tahir <arslan.tahir@outlook.com>
2022-09-16 13:53:21 +0500

    redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import LoginPage from './pages/LoginPage';
+import React, { Component } from 'react';
+import loginCtrl from './appCtrl'
 
-import NavBar from './components/NavBar/NavBar'
-import LoginPage from './pages/LoginPage'
 
 let Home=(<div>
   <h1>Home Page</h1>
@@ -11,17 +13,64 @@ let Home=(<div>
 
 
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends Component {
+  constructor(){
+
+
+    super();
+
+    // this.appCtrl=()=>{console.log(this)};
+    // this.appCtrl={...appCtrl()};
+    
+    // this.appCtrl();
+    console.log(loginCtrl)
+    this.loginCtrl={...loginCtrl}
+
+    this.state = {
+      login:{//name after compenent or page
+        userName: "test",
+        password: "",
+      }    
+    };
+
+  }
+
+  
+
+
+  render() { 
+    return (
+      <div className="App">
       <NavBar></NavBar>
       <Routes>
                 <Route path="/" element={Home} exact />
-                <Route path="/login" element={<LoginPage/>} />
+                
+                <Route 
+                  path="/login" 
+                  element={
+                    <LoginPage 
+                      login={this.state.login}
+                      loginCtrl={this.loginCtrl}/>
+                  }
+                />
+                
                 {/* <Route path="/shop" component={Shop} /> */}
       </Routes>
     </div>
-  );
+    );
+  }
 }
+ 
 
 export default App;
+
+
+
+
+
+
+
+
+
+
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,14 +2,28 @@ import React,{Component} from "react";
 import loginImage from "../media/login.gif"
 import Login from "../components/Login/Login"
 
+
+
+
+
 class LoginPage extends Component {
-    state = {  } 
+    state = {  }
+
+    componentDidMount(){
+        
+    }
+
     render() { 
+        
+
         return (
             <React.Fragment>
                 <div style={{display: 'flex',flexDirection: 'column',justifyContent: 'center',alignItems: 'center',height:'500px'}}>
                     {/* <span>LEGO</span> */}
-                    <Login></Login>
+                    <Login
+                        login={this.props.login}
+                        loginCtrl={this.props.loginCtrl}
+                    />             
                 </div>
             </React.Fragment>
         );
